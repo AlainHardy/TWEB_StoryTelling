b477549dcf8c6aed084027a26d061df03c37aa7b
Alain Hardy <al.hardy.16@gmail.com>
2017-12-01 11:24:41 +0100

    Add overview of all battles before the outro

--- app.js
+++ app.js
@@ -17,5 +17,12 @@ app.get('/', (req, res) => {
 
 app.listen(app.get('port'), () => {
   datas = JSON.parse(fs.readFileSync('data/NavalWarfare.json', 'utf8'));
+  datas.push({
+    "name": "Overview",
+    "location":{
+      "latitude":14.5615291,
+      "longitude":178.6969988
+    }
+  });
   console.log('Listen on port ', app.get('port'));
 });
